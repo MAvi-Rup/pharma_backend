6c301c21dfbb9a61b5663fe548f8a56374f247ba
MAvi-Rup <avirupmondal19@gmail.comm>
2023-01-17 13:51:14 +0600

    DOne

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
